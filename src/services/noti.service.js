73178e566d3f35b0392bee28b26eca87e3a0c777
dangwd <dangwd.vicidev@gmail.com>
2024-11-01 11:15:42 +0700

    update

---
+++ src/services/noti.service.js
@@ -0,0 +1,8 @@
+const notification = require("../models/notification.model")
+const { BadRequestError } = require("../core/error.response")
+
+class NotiService {
+  GetAll = async () => {
+    
+  }
+}
