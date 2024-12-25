dd4cf2a13aa703287f137bd8cdb365ae22e4430a
dangwd <dangwd.vicidev@gmail.com>
2024-12-25 08:31:44 +0700

    update

--- src/models/repo/project.repo.js
+++ src/models/repo/project.repo.js
@@ -1,23 +1,22 @@
 const projectModel = require("../project.model");
 
 const getAllProducts = async (userId, page, size, search) => {
-  const getAllProducts = async (userId, page, size, search) => {
-    const query = {
-      $or: [{ owner: userId }, { members: userId }],
-      ...(search ? { name: { $regex: search, $options: "i" } } : {}),
-    };
-
-    const data = await projectModel
-      .find(query)
-      .populate("columnIds")
-      .populate("members")
-      .populate("owner")
-      .skip((page - 1) * size) // Phân trang
-      .limit(size); // Giới hạn số lượng sản phẩm
-
-    const total = await projectModel.countDocuments(query); // Tổng số sản phẩm
-
-    return { data, total };
+  const query = {
+    $or: [{ owner: userId }, { members: userId }],
+    ...(search ? { name: { $regex: search, $options: "i" } } : {}),
   };
 
-  module.exports = { getAllProducts };
+  const data = await projectModel
+    .find(query)
+    .populate("columnIds")
+    .populate("members")
+    .populate("owner")
+    .skip((page - 1) * size) // Phân trang
+    .limit(size); // Giới hạn số lượng sản phẩm
+
+  const total = await projectModel.countDocuments(query); // Tổng số sản phẩm
+
+  return { data, total };
+};
+
+module.exports = { getAllProducts };
